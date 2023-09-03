ddb8b0ea56a20d77cf05b1934f06a3c9a2e0da1a
jkat74nti <johanerik.kateby@ntig.se>
2023-09-03 21:20:51 +0200

    Skapat projekt Övning 1 variabler

---
+++ src/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int tal1 = 2;
+        int tal2 =30;
+
+        int summa_addition = tal1+tal2;
+        int summa_subtraktion = tal1-tal2;
+
+        System.out.println("summan av tal + tal2 är "+summa_addition);
+        System.out.println("summan av tal  tal2 är "+summa_subtraktion);
+
+        int produkt=tal1*tal2;
+        int kvot = tal1/tal2;
+        
+        System.out.println("Produkten av tal1*tal2 är "+produkt);
+        System.out.println("Kvoten av tal1/tal2 är "+kvot);
+
+        int rest1 = 10%3;
+        int rest2 =9%3;
+
+        System.out.println("Resten av 10/3 är "+rest1);
+        System.out.println("Resten av 9/3 är "+rest2);
+        
+        double decimal_tal1 = 5.0;
+        double decimal_tal2= 23.41; 
+
+        double dec_summa_add = decimal_tal1+decimal_tal2; 
+        double dec_summa_sub =decimal_tal1-decimal_tal2;
+
+        System.out.println("Produkten av tal1*tal2 är "+produkt);
+        System.out.println("Kvoten av tal1/tal2 är "+kvot);
+
+    }
+}

f23238788130e9035bd0d143a24d2a263ebdc5c2
jkat74nti <johanerik.kateby@ntig.se>
2023-09-03 21:31:47 +0200

    Laggt till kod för heltals operationer

--- src/App.java
+++ src/App.java
@@ -29,6 +29,7 @@ public class App {
 
         System.out.println("Produkten av tal1*tal2 är "+produkt);
         System.out.println("Kvoten av tal1/tal2 är "+kvot);
+        System.out.println("SLUT");
 
     }
 }
